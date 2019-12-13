0ed029df86a6ae306653e25a07e37217fe843b80
Michael Lowe <mrlowememphis@yahoo.com>
2019-12-12 22:02:40 -0600

    MVP met

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,9 +1,32 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import * as rtl from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 import App from './App';
+import Dashboard from './components/Dashboard'
+import Display from './components/Display'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+
+test('renders without crashing', () => {
+  const wrapper = rtl.render(<App/>)
+  expect(wrapper).toBeVisible
 });
+
+
+test('renders dashboard component', () => {
+  const wrapper = rtl.render(<Dashboard/>)
+  const element = wrapper.getByText(/hit/i)
+  expect(element).toBeInTheDocument()
+}) 
+
+
+
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
